32b4012059e0cb512a1ee5dd8138148abb7573e2
Jeremy A Kansas <ymerejsasnak@gmail.com>
2014-11-06 10:11:57 -0500

    working on display

--- hangman.rb
+++ hangman.rb
@@ -1,5 +1,31 @@
 class Hangman
+	attr_accessor :wrong_guesses
+  attr_reader :current_word, :dictionary
 
+  def initialize
+  	@dictionary = Dictionary.new
+    @answer_word = dictionary.pick_word.split
+    @right_letters = []
+    @wrong_letters = []
+    @wrong_guesses = 0
+  end
+
+  def main_display
+    puts
+    puts
+    puts "Wrong guesses left: #{(6 - wrong_guesses)}"
+    puts
+    puts
+    puts "Word:"
+    puts
+    display_word
+    puts
+    puts
+  end
+
+  def display_word
+    current_word.each {print "_ "}
+  end
 
 
 end
@@ -13,7 +39,6 @@ class Dictionary
   	File.open("5desk.txt", "r") do |file|
     	@word_list = file.read.split("\r\n").select {|word| (5..12) === word.length}
     end
-
   end
 
   def pick_word
@@ -27,3 +52,9 @@ end
 class Player
 
 end
+
+
+
+
+
+game = Hangman.new
